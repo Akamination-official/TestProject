4b5147628001176b0c820332d94ea1afeee556a2
Akamination <2630102550@qq.com>
2026-05-08 23:22:03 +0800

    三层分发

--- classes.h
+++ classes.h
@@ -4,90 +4,90 @@
 
 class Class1 {
 public:
-    void PrintName() { std::cout << "Class1" << std::endl; }
+  void PrintName() { std::cout << "Class1" << std::endl; }
     void PrintNum() { std::cout << 1 << std::endl; }
 };
 
-class Class2 {
-public:
+class C
+  ic:
     void PrintName() { std::cout << "Class2" << std::endl; }
     void PrintNum() { std::cout << 2 << std::endl; }
 };
 
-class Class3 {
+  s Class3 {
 public:
     void PrintName() { std::cout << "Class3" << std::endl; }
     void PrintNum() { std::cout << 3 << std::endl; }
 };
-
+  
 class Class4 {
 public:
     void PrintName() { std::cout << "Class4" << std::endl; }
-    void PrintNum() { std::cout << 4 << std::endl; }
-};
+    voi
+  
 
 class Class5 {
 public:
-    void PrintName() { std::cout << "Class5" << std::endl; }
-    void PrintNum() { std::cout << 5 << std::endl; }
+    voi
+  void PrintNum() { std::cout << 5 << std::endl; }
 };
 
 class Class6 {
 public:
-    void PrintName() { std::cout << "Class6" << std::endl; }
+  void PrintName() { std::cout << "Class6" << std::endl; }
     void PrintNum() { std::cout << 6 << std::endl; }
 };
 
-class Class7 {
-public:
+class C
+  ic:
     void PrintName() { std::cout << "Class7" << std::endl; }
     void PrintNum() { std::cout << 7 << std::endl; }
 };
 
-class Class8 {
+  s Class8 {
 public:
     void PrintName() { std::cout << "Class8" << std::endl; }
     void PrintNum() { std::cout << 8 << std::endl; }
 };
-
+  
 class Class9 {
 public:
-    void PrintName() { std::cout << "Class9" << std::endl; }
-    void PrintNum() { std::cout << 9 << std::endl; }
+    void Prin tame() { std::cout << "Class9" << std::endl; }
+    void Prin tum() { std::cout << 9 << std::endl; }
 };
-
-class Class10 {
+ 
+class Class10  
 public:
-    void PrintName() { std::cout << "Class10" << std::endl; }
-    void PrintNum() { std::cout << 10 << std::endl; }
-};
+    void Prin tame() { std::cout << "Class10" << std::endl; }
+    void Prin tum() { std::cout << 10 << std::endl; }
+}; 
 
-extern Class1* c1_1;
-extern Class1* c1_2;
+extern Class1 *c1_1;
+extern Class1 *c1_2;
 
-extern Class2* c2_1;
-extern Class2* c2_2;
-
+extern Class2 *c2_1;
+extern Class2 *c2_2;
+ 
 extern Class3* c3_1;
-extern Class3* c3_2;
-extern Class3* c3_3;
-
+extern Class3 *c3_2;
+extern Class3 *c3_3;
+ 
 extern Class4* c4_1;
-extern Class4* c4_2;
-
-extern Class5* c5_1;
+extern Class4 *c4_2;
+ 
+extern Class5 *c5_1;
 extern Class5* c5_2;
-extern Class5* c5_3;
-
-extern Class6* c6_1;
+extern Class5 *c5_3;
+ 
+extern Class6 *c6_1;
 extern Class6* c6_2;
-extern Class6* c6_3;
-
-extern Class7* c7_1;
+extern Class6 *c6_3;
+ 
+extern Class7 *c7_1;
 extern Class7* c7_2;
-extern Class7* c7_3;
-
-extern Class8* c8_1;
+extern Class7*  7_3;
+ 
+extern Class8*  8_1;
 extern Class8* c8_2;
 extern Class8* c8_3;
 
