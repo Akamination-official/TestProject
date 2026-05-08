72f4c0863807df512ec1c0cbe07c1211ea5eddb1
Akamination <2630102550@qq.com>
2026-05-08 23:50:02 +0800

    更新.gitignore，移除不必要的构建产物

--- out/build/preset/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Project/TestProject
-# Build directory: D:/Project/TestProject/out/build/preset
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
