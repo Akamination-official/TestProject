72640f9a21a6f04c948191e6a0934e7eab270324
Akamination <2630102550@qq.com>
2026-05-09 08:18:42 +0800

    refactor(dispatcher): 重构分发逻辑以简化参数传递
    将分发函数的参数从传递整个Key对象改为直接传递索引值，减少参数传递的复杂度并提高代码可读性

--- dispatcher.cpp
+++ dispatcher.cpp
@@ -5,61 +5,61 @@
 namespace {
 
 template<typename Layer3Tuple, typename WrapperFunc, std::size_t... Is>
-bool dispatch_layer3_impl(Layer3Tuple& layer3, const core::Key& key, WrapperFunc wrapper, std::index_sequence<Is...>) {
-    using Func = bool(*)(Layer3Tuple&, const core::Key&, WrapperFunc);
+bool dispatch_layer3_impl(Layer3Tuple& layer3, int idx, WrapperFunc wrapper, std::index_sequence<Is...>) {
+    using Func = bool(*)(Layer3Tuple&, WrapperFunc);
     static const Func funcs[] = {
-        [](Layer3Tuple& t, const core::Key& k, WrapperFunc w) -> bool {
+        [](Layer3Tuple& t, WrapperFunc w) -> bool {
             w(std::get<Is>(t));
             return true;
         }...
     };
-    if (key.id3 >= 0 && key.id3 < sizeof...(Is)) {
-        return funcs[key.id3](layer3, key, wrapper);
+    if (idx >= 0 && idx < sizeof...(Is)) {
+        return funcs[idx](layer3, wrapper);
     }
     return false;
 }
 
 template<typename Layer3Tuple, typename WrapperFunc>
-bool dispatch_layer3(Layer3Tuple& layer3, const core::Key& key, WrapperFunc wrapper) {
-    return dispatch_layer3_impl(layer3, key, wrapper, std::make_index_sequence<std::tuple_size_v<Layer3Tuple>>{});
+bool dispatch_layer3(Layer3Tuple& layer3, int idx, WrapperFunc wrapper) {
+    return dispatch_layer3_impl(layer3, idx, wrapper, std::make_index_sequence<std::tuple_size_v<Layer3Tuple>>{});
 }
 
 template<typename Layer2Tuple, typename WrapperFunc, std::size_t... Is>
-bool dispatch_layer2_impl(Layer2Tuple& layer2, const core::Key& key, WrapperFunc wrapper, std::index_sequence<Is...>) {
+bool dispatch_layer2_impl(Layer2Tuple& layer2, int idx, const core::Key& key, WrapperFunc wrapper, std::index_sequence<Is...>) {
     using Func = bool(*)(Layer2Tuple&, const core::Key&, WrapperFunc);
     static const Func funcs[] = {
         [](Layer2Tuple& t, const core::Key& k, WrapperFunc w) -> bool {
-            return dispatch_layer3(std::get<Is>(t), k, w);
+            return dispatch_layer3(std::get<Is>(t), k.id3, w);
         }...
     };
-    if (key.id2 >= 0 && key.id2 < sizeof...(Is)) {
-        return funcs[key.id2](layer2, key, wrapper);
+    if (idx >= 0 && idx < sizeof...(Is)) {
+        return funcs[idx](layer2, key, wrapper);
     }
     return false;
 }
 
 template<typename Layer2Tuple, typename WrapperFunc>
-bool dispatch_layer2(Layer2Tuple& layer2, const core::Key& key, WrapperFunc wrapper) {
-    return dispatch_layer2_impl(layer2, key, wrapper, std::make_index_sequence<std::tuple_size_v<Layer2Tuple>>{});
+bool dispatch_layer2(Layer2Tuple& layer2, int idx, const core::Key& key, WrapperFunc wrapper) {
+    return dispatch_layer2_impl(layer2, idx, key, wrapper, std::make_index_sequence<std::tuple_size_v<Layer2Tuple>>{});
 }
 
 template<typename Layer1Tuple, typename WrapperFunc, std::size_t... Is>
-bool dispatch_layer1_impl(Layer1Tuple& layer1, const core::Key& key, WrapperFunc wrapper, std::index_sequence<Is...>) {
+bool dispatch_layer1_impl(Layer1Tuple& layer1, int idx, const core::Key& key, WrapperFunc wrapper, std::index_sequence<Is...>) {
     using Func = bool(*)(Layer1Tuple&, const core::Key&, WrapperFunc);
     static const Func funcs[] = {
         [](Layer1Tuple& t, const core::Key& k, WrapperFunc w) -> bool {
-            return dispatch_layer2(std::get<Is>(t), k, w);
+            return dispatch_layer2(std::get<Is>(t), k.id2, k, w);
         }...
     };
-    if (key.id1 >= 0 && key.id1 < sizeof...(Is)) {
-        return funcs[key.id1](layer1, key, wrapper);
+    if (idx >= 0 && idx < sizeof...(Is)) {
+        return funcs[idx](layer1, key, wrapper);
     }
     return false;
 }
 
 template<typename WrapperFunc>
 bool dispatch(const core::Key& key, WrapperFunc wrapper) {
-    return dispatch_layer1_impl(core::GetRootTuple(), key, wrapper, 
+    return dispatch_layer1_impl(core::GetRootTuple(), key.id1, key, wrapper, 
         std::make_index_sequence<std::tuple_size_v<core::RootTuple>>{});
 }
 
